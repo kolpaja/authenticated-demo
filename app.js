c11048059b1a93c95675bbd088c3bfdcfc1076ab
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-05-18 17:48:29 +0200

    initial commit of basic login/staylogin/logout form

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/authDemo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("db: we're connected!"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "notagoodsecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({ username, passwordHashed: hash });
+  await user.save();
+  req.session.user_id = user._id;
+  res.redirect("/secret");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcrypt.compare(password, user.passwordHashed);
+  if (validPassword) {
+    req.session.user_id = user._id;
+    res.redirect("/secret");
+  } else res.send("try again!");
+});
+
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  res.redirect("/login");
+});
+
+app.get("/secret", (req, res) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  res.render("secret");
+});
+
+app.listen(3000, () => console.log("Serving your app at port: 3000"));
